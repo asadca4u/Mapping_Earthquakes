0943d1bffd4289d00d5cc84ba1487f0ff814855b
Asad Khan <asad_ca4u@hotmail.com>
2020-09-29 11:13:15 -0400

    completed module 5.2

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -26,14 +26,12 @@ let sanFranAirport =
 
 // Grabbing our GeoJSON data.
 L.geoJson(sanFranAirport, {
-  // We turn each feature into a marker on the map.
-  pointToLayer: function(feature, latlng) {
-    console.log(feature);
-    return L.marker(latlng)
-    .bindPopup("<h2>" + feature.properties.city + "</h2>");
-  }
-
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup("<h2>" + feature.properties.city + "</h2>");
+   }
 }).addTo(map);
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',
 {
